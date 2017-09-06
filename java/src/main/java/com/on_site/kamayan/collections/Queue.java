22ac63f663b1087013baa8adaa37143572188612
Kylr Avery <kavery@on-site.com>
2017-09-06 13:48:28 -0700

    Finish queues

--- java/src/main/java/com/on_site/kamayan/collections/Queue.java
+++ java/src/main/java/com/on_site/kamayan/collections/Queue.java
@@ -17,31 +17,41 @@ public class Queue {
     }
 
     public Queue enqueue(Object value) {
-        throw Kamayan.todo(
-            "The enqueue(Object) method should add the argument to the end of",
-            "the queue, which should increase the size by 1. The return value",
-            "must be this."
-        );
+        // throw Kamayan.todo(
+        //     "The enqueue(Object) method should add the argument to the end of",
+        //     "the queue, which should increase the size by 1. The return value",
+        //     "must be this."
+        // );
+
+        list.add(value);
+
+        return this;
     }
 
     public Object dequeue() {
-        throw Kamayan.todo(
-            "The dequeue() method should remove and return the first value in",
-            "the queue. An IndexOutOfBoundsException should be thrown if the",
-            "Queue is empty."
-        );
+        // throw Kamayan.todo(
+        //     "The dequeue() method should remove and return the first value in",
+        //     "the queue. An IndexOutOfBoundsException should be thrown if the",
+        //     "Queue is empty."
+        // );
+
+        return list.deleteFirst();
     }
 
     public boolean isEmpty() {
-        throw Kamayan.todo(
-            "The isEmpty() method should return whether or not the size is 0."
-        );
+        // throw Kamayan.todo(
+        //     "The isEmpty() method should return whether or not the size is 0."
+        // );
+
+        return size() == 0;
     }
 
     public Object peek() {
-        throw Kamayan.todo(
-            "The peek() method should return the first value in the queue,",
-            "without removing any elements in the queue."
-        );
+        // throw Kamayan.todo(
+        //     "The peek() method should return the first value in the queue,",
+        //     "without removing any elements in the queue."
+        // );
+
+        return list.first();
     }
 }
